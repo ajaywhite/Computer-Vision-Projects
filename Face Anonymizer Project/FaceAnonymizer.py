67f32bb6c1596899489c8cb6448e9f6fe16b4cc3
Ajay A <93421952+ajaywhite@users.noreply.github.com>
2024-06-25 17:09:37 +0530

    Update FaceAnonymizer.py

--- Face Anonymizer Project/FaceAnonymizer.py
+++ Face Anonymizer Project/FaceAnonymizer.py
@@ -1,10 +1,12 @@
 import cv2
 import mediapipe as mp
 
+
+#Initialize MediaPipe face detection
 mp_face_detection = mp.solutions.face_detection
 face_detection = mp_face_detection.FaceDetection()
 
-
+#Open videocapture
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
@@ -14,11 +16,14 @@ while cap.isOpened():
         print(("No video"))
         break
 
+    #Get the dimension of frame
     H, W, _ = frame.shape
     print("The height and weight of the frame:",H, W)
 
+    #convert color BGR to RGB as MediaPipe require RGB input
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    #process the frame the face detection
     results = face_detection.process(frame_rgb)
     if results.detections:
         for detection in results.detections:
@@ -35,15 +40,17 @@ while cap.isOpened():
             #specify the region to face anonymizer
             roi = frame[y:y+h, x:x+w]
 
+            #blurring using GaussianBlur
             blurred_region = cv2.GaussianBlur(roi,(35, 35), 15)
 
+            #Back to the original frame
             frame[y:y+h, x:x+w] = blurred_region
 
             #Draw boundary box
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0 , 0), 2)
 
 
-
+    #Display the frame
     cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
